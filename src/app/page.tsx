2fb2361eb7b6fdf388cd61362570b7fef06f6dd0
Tom Seidel <hi@tomseidel.com>
2025-06-14 23:50:19 +0200

    feat: add basic landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl">Personal website for Tom Seidel</h1>
+      <p className="text-xl">Built with Next.js and Tailwind CSS</p>
+    </>
+  );
+}
